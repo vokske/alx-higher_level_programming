ef89dbbdc46ee39fd1877828b4f219f1a87c3d6a
vokske <kevinmuigai2@gmail.com>
2024-03-14 19:12:04 +0300

    ASCII alphabet except e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letters in range(ord('a'), ord('z') + 1):
+for letters in range(97, 123):
     if chr(letters) not in ['e', 'q']:
         print("{}".format(chr(letters)), end="")
