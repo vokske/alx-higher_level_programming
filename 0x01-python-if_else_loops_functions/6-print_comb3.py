04d0abbb75655f983cb6da2bd5a9014a099f10c8
vokske <kevinmuigai2@gmail.com>
2024-03-15 11:29:43 +0300

    Combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=", " if i < 0 or j < 0 else "\n")
+        print("{}{}".format(i, j), end=", " if i < 9 or j < 9 else "\n")
