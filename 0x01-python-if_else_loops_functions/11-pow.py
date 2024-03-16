01fdba0c3c2aa1871b081a52bebba8eb7532bba4
vokske <kevinmuigai2@gmail.com>
2024-03-16 17:26:13 +0300

    a to the power of b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    power = 1
-    if b == 0:
-        return 1
-    elif b < 0:
-        a = 1 / a
-    for _ in range(abs(b)):
-        power *= a
-    return power
+    return a ** b
