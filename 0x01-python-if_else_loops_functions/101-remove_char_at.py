9719be10e8622500fd82e33677a3e6620f3af629
vokske <kevinmuigai2@gmail.com>
2024-03-17 14:15:07 +0300

    Remove character at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -5,5 +5,7 @@ def remove_char_at(str, n):
     for i in range(len(str)):
         if i != n:
             str_copy += str[i]
-    print("{}".format(str_copy), end="")
+    print("{}".format(str_copy), end="\n")
     return str_copy
+
+remove_char_at('Chicago', 3)
