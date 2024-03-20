e3521413ac4d7a07ad40258e8dffa26eb2e0dc6e
vokske <kevinmuigai2@gmail.com>
2024-03-20 21:31:03 +0300

    Print arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+    args = sys.argv[1:]
+    counter = len(args)
+    if counter == 0:
+        print("{} arguments.".format(counter))
+    elif counter == 1:
+        print("{} argument:".format(counter))
+        print("{}: {}".format(counter, sys.argv[counter]))
+    else:
+        for i in range(counter):
+            print("{} arguments:".format(counter))
+            print("{}: {}".format(i + 1, args[i]))
+
