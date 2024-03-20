93285e5f13088ffd89b6e0aea9cb980fb44b9bf1
vokske <kevinmuigai2@gmail.com>
2024-03-20 13:18:44 +0300

    Import add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
-
 from add_0 import add
-sum = add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    sum = add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
