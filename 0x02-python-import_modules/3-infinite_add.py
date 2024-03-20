fbca128fd0cf71007ed052b5ae2d18434bbcb173
vokske <kevinmuigai2@gmail.com>
2024-03-20 23:21:18 +0300

    Infinite addition of arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,8 @@
 if __name__ == '__main__':
     import sys
     args = sys.argv[1:]
+    arg_counter = len(args)
     sum_args = 0
-    for i in range(len(args)):
-        sum_args += int(args[i + 1])
+    for i in range(arg_counter):
+        sum_args += int(args[i])
     print("{}".format(sum_args))
