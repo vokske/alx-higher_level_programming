e218c4cf571a85d0632a998fbaf8b8d85315917c
vokske <kevinmuigai2@gmail.com>
2024-03-21 16:49:59 +0300

    Build calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,31 +7,27 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    else:
-        try:
-            a = int(sys.argv[1])
-            operator = sys.argv[2]
-            b = int(sys.argv[3])
-        except ValueError:
-            print("Arguments must be integers")
-            sys.exit(1)
+    
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
 
-        valid_operator = {'+', '-', '*', '/'}
+    valid_operator = {'+', '-', '*', '/'}
 
-        if operator not in valid_operator:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-        if operator == '/' and b == 0:
-            print("Error: Division by zero")
-            sys.exit(1)
+    if operator not in valid_operator:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    if operator == '/' and b == 0:
+        print("Error: Division by zero")
+        sys.exit(1)
 
-        if operator == '+':
-            result = add(a, b)
-        elif operator == '-':
-            result = sub(a, b)
-        elif operator == '*':
-            result = mul(a, b)
-        elif operator == '/':
-            result = div(a, b)
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
 
-        print(f"{a} {operator} {b} = {result}")
+    print(f"{a} {operator} {b} = {result}")
