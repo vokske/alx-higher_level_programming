c19abe0216e25d11a31d451ae809f83622cac317
vokske <kevinmuigai2@gmail.com>
2024-03-21 16:53:22 +0300

    Build calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
