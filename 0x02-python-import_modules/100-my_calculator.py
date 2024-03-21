e917a589b85043e5532ac7fb9f47def8b7807145
vokske <kevinmuigai2@gmail.com>
2024-03-21 16:31:09 +0300

    Build calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,9 +4,14 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    try:
+        a = int(sys.argv[1])
+        operator = sys.argv[2]
+        b = int(sys.argv[3])
+    except ValueError:
+        print("Arguments must be integers")
+        sys.exit(1)
+
     valid_operator = {'+', '-', '*', '/'}
        
     if len(sys.argv) != 4:
@@ -16,7 +21,10 @@ if __name__ == "__main__":
     if operator not in valid_operator:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    
+    if operator == '/' and b == 0:
+        print("Error: Division by zero")
+        sys.exit(1)
+
     if operator == '+':
         result = add(a, b)
     elif operator == '-':
