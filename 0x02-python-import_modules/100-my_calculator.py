d714c6d8da16dfdb94a6f61e454faad8838317ad
vokske <kevinmuigai2@gmail.com>
2024-03-21 16:18:51 +0300

    Build calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 
+import sys
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-    def calculate:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        operator = sys.argv[2]
-        valid_operator = ('+', '-', '*', '/')
-        if len(sys.argv) != 4:
-            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-            sys.exit(1)
-        if operator not in valid_operator:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-        elif operator == '+':
-            return add(a, b)
-        elif operator == '-':
-            return sub(a, b)
-        elif operator == '*':
-            return mul(a, b)
-        elif operator == '/':
-            return div(a, b)
-        result = calculate(a, operator, b)
-        print(f"{a} {operator} {b} = {result}")
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    valid_operator = {'+', '-', '*', '/'}
+       
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+        
+    if operator not in valid_operator:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    
+    print(f"{a} {operator} {b} = {result}")
