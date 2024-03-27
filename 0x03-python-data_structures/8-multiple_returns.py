ea422194bf9d33e98ea14b7d4feac596fde1d249
vokske <kevinmuigai2@gmail.com>
2024-03-27 12:39:18 +0300

    Multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,4 @@
 def multiple_returns(sentence):
     if not sentence:
         return (len(sentence), None)
-    else:
-        return(len(sentence), sentence[0])
+    return(len(sentence), sentence[0])
