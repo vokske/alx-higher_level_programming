1d4567477d76079cc334441594a8f1d7fe5b0daa
vokske <kevinmuigai2@gmail.com>
2024-04-02 22:45:20 +0300

    Search and replace in list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in my_list:
+        if i == search:
+            my_list.append(replace)
+        else:
+            my_list.append(i)
+    return new_list
