c9aa4cdf12f187d0f06b4a6fd79abdc11406d2f3
vokske <kevinmuigai2@gmail.com>
2024-04-02 18:05:23 +0300

    Square all matrix elements

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    square_matrix = list(map(lambda row: list(map(lambda i:i**2, row)), matrix))
-    return square_matrix
+    sq_matrix = list(map(lambda row: list(map(lambda i: i**2, row)), matrix))
+    return sq_matrix
