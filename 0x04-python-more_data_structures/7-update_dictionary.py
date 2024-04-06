a6205af7192ba1c5198740046f6e1a53bd882075
vokske <kevinmuigai2@gmail.com>
2024-04-06 12:32:34 +0300

    Update a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    dict_1 = dict([key, value])
-    a_dictionary.update(dict_1)
-    print(a_dictionary)
+    update_dictionary[key] = value
+    return update_dictionary
