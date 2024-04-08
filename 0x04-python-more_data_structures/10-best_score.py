e647bba7b716cb9d440af84f9b17d73204dad52d
vokske <kevinmuigai2@gmail.com>
2024-04-08 11:32:36 +0300

    Find biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
+    max_score = 0
     if a_dictionary:
-        max_score = 0
         for keys in a_dictionary:
             if a_dictionary[keys] > max_score:
                 max_score = a_dictionary[keys]
