f0ac48b1481e1e233ef5f5d7f226cb9c3a3cfed7
vokske <kevinmuigai2@gmail.com>
2024-04-09 19:53:52 +0300

    Print multiple lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavaScript is amazing\n');
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+console.log(line1);
+console.log(line2);
+console.log(line3);
