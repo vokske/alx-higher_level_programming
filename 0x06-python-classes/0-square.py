289422660bb58efdba64a5ffcad568a561414958
vokske <kevinmuigai2@gmail.com>
2024-04-18 17:04:21 +0300

    Empty class square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,15 @@
 class Square:
     """
     Empty class representing a square
+    
+    Attributes:
+        (None)
+
+    Methods:
+        (None)
+
+    Usage:
+        Create an instance of the class Squareto represent a square
     """
     pass
+
