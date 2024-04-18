5ae82c4d9fed48b8a5b1c7225517714835853dc8
vokske <kevinmuigai2@gmail.com>
2024-04-18 09:57:20 +0300

    Printing list with try/except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for element in range(x):
-            print(element, end="")
+        while count < x:
+            print(my_list[count], end="")
             count += 1
     except IndexError:
         break
