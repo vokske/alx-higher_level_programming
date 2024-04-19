0f67bb252ffecc9fb4ed02e76c529b4988ecb6f8
vokske <kevinmuigai2@gmail.com>
2024-04-19 11:57:27 +0300

    Validate size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+Represents a square
+"""
+
+class Square:
+    """
+    Defines a square
+    
+    Attributes:
+        Size(int): The length of each side of the square.
+
+    Methods:
+        (None)
+
+    Usage:
+        Create an instance of the Square class.
+    """
+
+    def __init__(self, size=0)
+        """
+        Initializes a Square instance with the provided size
+        """
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if value.isdigit():
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("Size must be >= 0")
+        self.__size = size
