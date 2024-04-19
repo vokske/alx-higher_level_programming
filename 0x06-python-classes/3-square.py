71ca54bb7e470cfa44e2c0b68236fb3832348bf8
vokske <kevinmuigai2@gmail.com>
2024-04-19 22:07:06 +0300

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,9 +4,7 @@
 
 class Square:
     """Represents a square."""
-    
-
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
         Initializes a Square instance.
 
@@ -19,8 +17,8 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self._Square__size = size
+        self.__size = size
 
     def area(self):
         """Returns the area of a square."""
-        return self._Square__size ** 2
+        return self.__size ** 2
