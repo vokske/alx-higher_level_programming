5cc32943f7a375252c0ba75097b5c9fda9dda16c
vokske <kevinmuigai2@gmail.com>
2024-04-20 20:24:16 +0300

    Getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance.
+
+
+        Args:
+            Size(int): The size of the side of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Sets the value for size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of a square."""
+        return self.__size ** 2
