08eedc024e1630ac8b40b8611feea1c9c89e6287
vokske <kevinmuigai2@gmail.com>
2024-04-20 21:01:01 +0300

    Getter and setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,14 +17,14 @@ class Square:
     @property
     def size(self):
         """Retrieves the size"""
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
         """Sets the value for size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
