cf658f98b4d7058217918e8a4ab9150cc4b9a208
vokske <kevinmuigai2@gmail.com>
2024-04-22 16:03:16 +0300

    Repr for eval

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -149,5 +149,5 @@ class Rectangle:
         Returns:
             str: String representation of the rectangle.
         """
-        return("{}({}, {})".format(self.__class__.__name__,
-                                   self.__width, self.__height))
+        return("{}({}, {})".format(self.__class__.__name__, self.__width,
+               self.__height))
