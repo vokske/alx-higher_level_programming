7eec39baa5ea3d005d9ed204daf11d697cd5402e
vokske <kevinmuigai2@gmail.com>
2024-04-23 11:41:06 +0300

    Change representation of #

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -135,7 +135,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return ("\n".join([str(self.print_symbol) * self.__width]
-                * self__height))
+                * self.__height))
 
     def __repr__(self):
         """String repr of rectangle class.
