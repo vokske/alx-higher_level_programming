05936b06e153361d68acf236ac42224c3b5c0e24
vokske <kevinmuigai2@gmail.com>
2024-04-23 13:57:04 +0300

    Square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -67,7 +67,22 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """Returns a new Rectangle instance woth width == height == size"""
+        """Creates and returns a new Rectangle instance representing a square.
+
+        Args:
+            size (int): The width and height of the square. Default is 0.
+
+        Returns:
+            Rectangle: A new Rectangle instance representing a square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        if size < 0:
+            raise ValueError("Size must be >= 0")
         return cls(size, size)
 
     def __init__(self, width=0, height=0):
