d4aa632c3737c687b15e62c492a9a1757b8b2c10
vokske <kevinmuigai2@gmail.com>
2024-05-02 17:06:29 +0300

    Divide matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,8 +16,10 @@ def matrix_divided(matrix, div):
     Returns: A new matrix containing the quotients of original matrix.
     """
 
-    if not all(isinstance(row, list) and all(isinstance(element, (int, float)) for element in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) and all(isinstance(element, (int, float))
+               for element in row) for row in matrix):
+        raise TypeError("matrix must be a matrix(list of lists) of
+                        integers/floats")
     row_len = set(len(row) for row in matrix)
     if len(row_len) != 1:
         raise TypeError("Each row of the matrix must have the same size")
@@ -25,5 +27,6 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    result_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    result_matrix = [[round(element / div, 2) for element in row]
+                     for row in matrix]
     return result_matrix
