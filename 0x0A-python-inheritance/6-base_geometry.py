78fa5b4a4533f4c53a97649f55c2a0040787a387
vokske <kevinmuigai2@gmail.com>
2024-05-14 01:45:20 +0300

    BaseGeometry with a method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module contains class BaseGeometry."""
+
+
+class BaseGeometry:
+    """
+    Class named BaseGeometry.
+
+    Methods:
+        area.
+    """
+    def area(self):
+        """
+        Area method.
+
+        Raises:
+            Exception with custom message.
+        """
+        raise Exception("area() is not implemented")
