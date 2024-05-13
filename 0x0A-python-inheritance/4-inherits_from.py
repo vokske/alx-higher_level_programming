e30317709714a2d45a446b7a67a6d3a3ad8c5d47
vokske <kevinmuigai2@gmail.com>
2024-05-14 01:15:32 +0300

    Only sun-class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,4 +14,4 @@ def inherits_from(obj, a_class):
     Returns:True if object is an instance of a class that inherited from
             specified class. False otherwise.
     """
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
