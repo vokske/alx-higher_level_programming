639f2d9848d27e6ca4691258a4b9743178f17da9
vokske <kevinmuigai2@gmail.com>
2024-05-14 00:45:15 +0300

    Only sun-class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module contains function inherits_from."""
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks if object is an instance of a class that inherited from the
+    specified class.
+
+    Args:
+        obj: Object.
+        a_class: Specified class.
+
+    Returns:True if object is an instance of a class that inherited from
+            specified class. False otherwise.
+    """
+    return issubclass(obj, a_class)
