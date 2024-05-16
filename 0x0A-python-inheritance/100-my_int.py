94ad5026e89eaf3d737ec23d455cb35f0309e17f
vokske <kevinmuigai2@gmail.com>
2024-05-16 20:30:24 +0300

    My Integer comparison

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module contains class MyInt."""
+
+
+class MyInt(int):
+    """
+    Inverts the operations of '==' and '!='
+
+    Methods:
+        __eq__()
+        __ne__()
+    """
+
+    def __eq__(self, other):
+        return super.__ne__(other)
+
+    def __ne__(self, other):
+        return super.__eq__(other)
