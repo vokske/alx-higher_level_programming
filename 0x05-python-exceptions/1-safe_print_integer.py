8b597ccb6560d78290e6a71a9621faa51827ff69
vokske <kevinmuigai2@gmail.com>
2024-05-16 23:28:44 +0300

    Safe printing of integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     count = 0
     try:
-        print("{:d}.format(value)")
+        print("{:d}".format(value))
         print()
         return True
     except TypeError:
