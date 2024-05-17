77c691eb2b795317431606ca31f7d4e602e2cb3b
vokske <kevinmuigai2@gmail.com>
2024-05-17 14:01:42 +0300

    ByteCode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,6 +7,6 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             result += (a ** b) / i
         except:
-            result += a + b
+            result = a + b
             break
     return result
