59f12a9a5a2952c1096b3dc364fc9eb2089ef1ee
vokske <kevinmuigai2@gmail.com>
2024-05-17 19:03:15 +0300

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,12 @@
 
 
 def write_file(filename="", text=""):
+    """
+    Writes to a file or creates it if it doesn't exist.
+
+    Args:
+        filename: Name of the file.
+        text: String to be written onto the file.
+    """
     with open(f"{filename}", mode="w", encoding="utf-8") as testFile:
         return testFile.write(f"{text}", end="")
