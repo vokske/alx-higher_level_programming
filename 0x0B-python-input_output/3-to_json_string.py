629dc7335874843cf210a464c377997d728051f5
vokske <kevinmuigai2@gmail.com>
2024-05-17 21:29:50 +0300

    Convert to JSON stringwq

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module contains the to_json_string function."""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Gives the JSON representation of an object.
+
+    Args:
+        my_obj: Specified object.
+    """
+    json.dumps(my_obj)
