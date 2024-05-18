1eb41bab9e10ecf02a22bc59b727ef0408d6b1c1
vokske <kevinmuigai2@gmail.com>
2024-05-18 16:04:44 +0300

    Dictionary description for object

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module contains the class_to_json function."""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description withsimple data structure.
+
+    Args:
+        obj: Specified Python object.
+    """
+    return obj.__dict__
