f416611ecd9e0ec292e5109971da4d209be86267
vokske <kevinmuigai2@gmail.com>
2024-05-19 22:49:09 +0300

    Pascal's triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -21,6 +21,4 @@ def pascal_triangle(n):
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
-    for row in triangle:
-        print(row)
     return triangle
