84402e22f14902fe9d417222439e94d047471eed
vokske <kevinmuigai2@gmail.com>
2024-05-20 14:03:30 +0300

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module contains the class Base."""
+
+
+class Base(object):
+    """
+    Manages the id attribute.
+
+    Private class attribute: __nb_objects.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialization of the Base class.
+
+        Args:
+            id: id of the instance. Initialized to "None".
+        """
+
+        if id is not None:
+            self.id = id
+        __nb_objects += 1
+        self.id = Base.__nb_objects
