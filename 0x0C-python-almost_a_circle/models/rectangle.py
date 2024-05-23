352e4eef4b173be7b7fb6f3c4318961468959f82
vokske <kevinmuigai2@gmail.com>
2024-05-23 19:05:26 +0300

    Update Rectangle with kwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -113,15 +113,20 @@ class Rectangle(Base):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - \
 {self.width}/{self.height}"
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
-        Assign an argument to each attribute.
+        Assign a key/value argument to attributes.
 
         Args:
             *args: Valriable length argument list in order.
         """
-        attrs = ["id", "width", "height", "x", "y"]
 
-        for i, arg in enumerate(args):
-            if i < len(attrs):
-                setattr(self, attrs[i], arg)
+        if args and len(args) != 0:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i, arg in enumerate(args):
+                if i < len(attrs):
+                    setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
