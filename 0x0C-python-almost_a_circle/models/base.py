b0563eb0ca42f3f8a791188bfbab2a8f6d46a308
vokske <kevinmuigai2@gmail.com>
2024-05-24 18:50:55 +0300

    JSON string to list of dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,6 +46,15 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        """
+        Convert json_string to a list of dictionaries.
+
+        Args:
+            json_string: A string representing a list of dictionaries
+
+        Returns: The list of the JSON string representation json_string.
+        """
+
         if json_string is None or len(json_string) == 0:
             return []
         else:
