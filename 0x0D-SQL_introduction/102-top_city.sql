87a617a3522cfe3bda744705b03b34f9a3867a7d
vokske <kevinmuigai2@gmail.com>
2024-07-27 19:52:58 +0300

    Limit 3

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,4 @@
 -- Imports table dump to database
 -- Displays the top three cities by average temp during July & August
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
 
