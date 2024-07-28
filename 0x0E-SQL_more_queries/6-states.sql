f13c08c8aeb739b8285090612a5086a8d6250c38
vokske <kevinmuigai2@gmail.com>
2024-07-28 22:40:07 +0300

    ID as Primary Key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates a database and table on my MYSQL server
+-- Values in 'id' column must be unique, auto-generated, and can't be null.
+-- The id value is to be used as the primary key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_GENERATE UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
+
