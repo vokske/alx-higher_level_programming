3a8ce7e14b1a60c2b6c259d96f9e82bed41845de
vokske <kevinmuigai2@gmail.com>
2024-07-28 22:01:45 +0300

    Create user and grant privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
 -- Sets the password for the user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO user_0d_1@localhost;
 
