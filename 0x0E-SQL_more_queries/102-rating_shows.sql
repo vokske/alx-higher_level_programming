51ec5518884ee3daf485dfb614833d6ae6b83614
vokske <kevinmuigai2@gmail.com>
2024-08-01 23:15:15 +0300

    List all shows by rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,7 +2,7 @@
 -- List all shows from hbtn_0d_tvshows_rate by their rating
 -- Each record should display tv_shows.title - rating sum
 -- Results are sorted in descending order by the rating
-SELECT title, SUM(tv_show_ratings.rate) AS 'rating sum' FROM tv_shows
+SELECT title, SUM(tv_show_ratings.rate) AS 'rating' FROM tv_shows
 JOIN tv_show_ratings
 ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
