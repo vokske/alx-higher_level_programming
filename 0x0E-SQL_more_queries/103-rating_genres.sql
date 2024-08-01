02e23cde0e2d7323657a5a786ddd20ee9b805e7d
vokske <kevinmuigai2@gmail.com>
2024-08-01 23:42:34 +0300

    List all genres by their rating

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,10 @@
+-- List all genres by their rating
+-- Each record displays tv_genres.name - rating
+-- Results are sorted in descending order by the rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS 'rating'
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
+
