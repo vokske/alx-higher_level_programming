650956546acf4ad2e27750581ce497e277c4d0ca
vokske <kevinmuigai2@gmail.com>
2024-10-17 17:24:43 +0300

    Filter and list states containing 'a'

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}')
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
