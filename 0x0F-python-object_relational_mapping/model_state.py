82ddbfacea455e8934277e85f33f0848f7b3403f
vokske <kevinmuigai2@gmail.com>
2024-10-17 14:55:22 +0300

    Base class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python3
 
-"""File contains the class definition of a State and an instance of Base class."""
-
+import sys
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import Column, Integer, String
 
 Base = declarative_base()
 
 class State(Base):
+    __table__name = "states"
 
-   """
-   Attributes:
-
-   __tablename__ (str): Name of the MySQL table.
-   
-   id (int): Unique identification number for a state.
-   
-   name (str): Name of a state.
-   """
-
-    __tablename__ = "states"
-
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
