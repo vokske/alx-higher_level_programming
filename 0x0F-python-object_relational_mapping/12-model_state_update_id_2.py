9acb7dfc92fcd1661db5a6f3c1da175602da379a
vokske <kevinmuigai2@gmail.com>
2024-10-18 14:49:53 +0300

    Update name attr of State object

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}')
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    session.query(State).filter(State.id == 1).update({State.name: 'New Mexico'}, synchronize_session=False)
+
+    session.commit()
+
